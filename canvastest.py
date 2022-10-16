6d8eecf0ba92cd37f4a2b6bac11cd1d06a57f9d0
Ryan Lee <ryanlee619@gmail.com>
2022-10-15 21:06:33 -0700

    Co-authored-by: Sal Saygi <salih.saygie@gmail.com> Co-authored-by: Andres Torres <Ahsoka@users.noreply.github.com>

--- canvastest.py
+++ canvastest.py
@@ -1,10 +1,56 @@
 # Import the Canvas class
 from canvasapi import Canvas
+import requests
+from markdownify import markdownify as md
+from discord import Bot
+from pprint import pprint
 
-# Canvas API URL
-API_URL = "https://ucmerced.instructure.com/api/v1/courses/25052/assignment_groups?exclude_assignment_submission_types[]=wiki_page&exclude_response_fields[]=description&exclude_response_fields[]=rubric&include[]=assignments&include[]=discussion_topic&override_assignment_dates=true&per_page=50"
-# Canvas API key
-API_KEY = "p@$$w0rd"
+import discord
+import functools
 
-# Initialize a new Canvas object
-canvas = Canvas(API_URL, API_KEY)
+import base64
+bot = Bot(intents=discord.Intents(guilds=True))
+# bot = Bot(intents=discord.Intents(guilds=True, members=True))
+
+discord_md = functools.partial(md, convert=['b', 'i', 'blockquote', 'code', 'a', 's', ''])
+md(convert=[''])
+discord_md()
+
+@bot.event
+async def on_ready():
+    # Canvas API URL
+    API_URL = "https://ucmerced.instructure.com/"
+    # Canvas API key
+    API_KEY = "1101~DZsUqsZ9lEJx1YuHHg1VF1jCX8d0FSznxrMAO4OvHqOdFejPjEPnMjA2HTsY6Shl"
+
+    # Initialize a new Canvas object
+    canvas = Canvas(API_URL, API_KEY)
+    courses = canvas.get_courses(enrollment_state="active")
+    # announcements = canvas.get_announcements([*courses])
+    announcements = canvas.get_announcements(list(courses))
+    # print(bot.guilds)
+    guild = bot.get_guild(810742455745773579)
+    # print(announcements)
+    for ann in announcements:
+        # print(md(ann.message))
+        await bot.get_guild(810742455745773579).get_channel(945195805688594502).send(md(ann.message))
+
+
+
+
+# print(courses)
+# group = Group()
+# groups = canvas.get_groups()
+# print(groups)
+# pprint(canvas.get_announcements(context_codes = courses))
+# for course in courses:
+#     print(type(course))
+#     print(canvas.get_announcements(course))
+    # pprint(course.get_gradebook_history_dates)
+    # print()
+
+# get_settings
+
+# bot.run('NzIyOTYxNDU2MzExNjk3NTEw.XuxSKA._-Jf3n9mos1H7p5r-4uptubJcqs')
+
+bot.run(base64.b64decode(b'T0RFNE5qZ3pOak0zT1RNMU16YzBNelUyLllFYm9qUS40ZkY4d3VVRDduaE1KVU5QSFVjN1kwTFFhUlU=').decode())
